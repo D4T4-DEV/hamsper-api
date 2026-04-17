38ce6005b082cd74aefa90fd5c7fb825b70bd859
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2026-04-16 21:43:28 -0600

    Update app.ts
    Uso del middleware para transformar los request a json

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import AppRouter from "./routes/router";
+import { handleError } from "./middlewares/handleError";
 
 const app = express();
 
 // Middlewares globales
-// app.use(express.json());
+
+// Devuelve un middleware que solo analiza JSON y solo examina las solicitudes donde
+// el encabezado Content-Type coincide con la opción de tipo.
+app.use(express.json());
 
 // Ruteo de la API
 app.use("/", AppRouter);
+
+// Middlewares de manejo de errores
+app.use(handleError);
 
 export default app;
