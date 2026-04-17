881385b49ec8edcf997187a6241b4ae6c31c73be
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2026-04-16 22:59:34 -0600

    Update env-container.ts

--- src/core/env-container.ts
+++ src/core/env-container.ts
@@ -1,1 +1,11 @@
 export const PORT = Number(process.env.PORT) || 3002;
+
+export const POSTGRES_PORT = Number(process.env.POSTGRES_PORT) || 5432;
+export const POSTGRES_CONECTIONS =
+  Number(process.env.POSTGRES_CONECTIONS) || 20;
+export const POSTGRES_IDLE_CONECTIONS =
+  Number(process.env.POSTGRES_IDLE_CONECTIONS) || 2000;
+export const POSTGRES_USER = process.env.POSTGRES_USER;
+export const POSTGRES_HOST = process.env.POSTGRES_HOST;
+export const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE;
+export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
