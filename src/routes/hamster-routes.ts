53f0e144b4658706bc04d86e9199191fcc076b8b
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2026-04-16 22:12:29 -0600

    Update hamster-routes.ts
    Documentación y nuevo endpoint

--- src/routes/hamster-routes.ts
+++ src/routes/hamster-routes.ts
@@ -1,17 +1,26 @@
 import express from "express";
+import * as hamsterController from "../controllers/HamsterController";
 
 const HamsterRouter = express.Router();
 
 // GET /hamster/
-HamsterRouter.get("/");
+// Obtiene todos los hamsters de la comunidad, en formato listado, no necesitas clave API
+HamsterRouter.get("/", hamsterController.getAllHamsters);
+
+// GET /hamster/
+// Obtiene solo los hamsters creados con la clave API, pero necesita tener clave API
+HamsterRouter.get("/my-hamsters", hamsterController.getAllHamsters);
 
 // POST /hamster/
-HamsterRouter.post("/");
+// Crea un nuevo hamster, pero necesita tener clave API
+HamsterRouter.post("/", hamsterController.createHamster);
 
 // PATCH /hamster/
-HamsterRouter.patch("/");
+// Actualiza un hamster registrado por la clave API, pero necesita tener clave API
+HamsterRouter.patch("/:hamsterId", hamsterController.updateHamster);
 
 // DELETE /hamster/
-HamsterRouter.delete("/");
+// Elimina un hamster registrado por la clave API, pero necesita tener clave API
+HamsterRouter.delete("/:hamsterId", hamsterController.deleteHamster);
 
 export default HamsterRouter;
